f3a27ba5b704ada17a87a3bac0d8c8a3a5f6a81d
Xun Huang <xunhuang1995@gmail.com>
2020-09-29 15:03:54 -0500

    Update build_all_dockers.sh

--- scripts/build_all_dockers.sh
+++ scripts/build_all_dockers.sh
@@ -5,7 +5,7 @@ y="20"
 for m in 05 06 07 08
 do
   rm -rf Dockerfile
-  echo "FROM nvcr.io/nvidian/pytorch:${y}.${m}-py3" > Dockerfile
+  echo "FROM nvcr.io/nvidia/pytorch:${y}.${m}-py3" > Dockerfile
   input="Dockerfile.base"
   while IFS= read -r line
   do
